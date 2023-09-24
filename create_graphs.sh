71add78973888be5c269e8c08699c2976d365b6f
Josh <kyte79@hotmail.com>
2023-09-24 13:59:47 -0400

    Sleep

--- create_graphs.sh
+++ create_graphs.sh
@@ -4,9 +4,14 @@ install_dir="${HOME}/.local/share/safe/tools/rewards_plotting"
 
 cd $install_dir
 
-#Activate virtual environment
+# Activate virtual environment
 source $install_dir/RPvenv/bin/activate
 
 python3 ./interactive_rewards.py
+
+sleep 10
 python3 ./interactive_memory.py
+
+sleep 10
 python3 ./bubble_rewards.py
+
