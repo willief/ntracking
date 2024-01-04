314e35d4083805e9f689fe6cd4fec4f4021c550b
Josh Clsn <59794857+javages@users.noreply.github.com>
2024-01-04 09:38:02 -0500

    Add files via upload

---
+++ mtracking/machine_logs_scp.sh
@@ -0,0 +1,5 @@
+scp wyse1@192.168.254.152:~/machine_resources.log /home/wyse/ntracking/Wyse1.log
+scp wyse2@192.168.254.153:~/machine_resources.log /home/wyse/ntracking/Wyse2.log
+scp wyse3@192.168.254.154:~/machine_resources.log /home/wyse/ntracking/Wyse3.log
+scp wyse4@192.168.254.158:~/machine_resources.log /home/wyse/ntracking/Wyse4.log
+cp $HOME/machine_resources.log /home/wyse/ntracking/Wyse.log
