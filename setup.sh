acd467b04f241cb58b6c6046afdc02c92e3f95dd
Neik <25412853+neilgcns@users.noreply.github.com>
2024-02-06 05:25:42 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -81,18 +81,24 @@ echo "installing matplotlib"
 sleep 2
 pip3 install matplotlib
 
-#clear
-#echo "jinga"
-#sleep 2
-#pip3 install matplotlib
+clear
+echo "jinja2"
+sleep 2
+pip install jinja2
 
 #!/usr/bin/env bash
 
 ######################## install vnstat
+clear
+echo "installing vnstat"
+sleep 2
 sudo apt install vnstat
 whiptail --msgbox --title "installation of vnstat complete" "if you have more than one network adapter you must remove all network adapters except the primary internet connection that the nodes use to connect to the internet use the following command.\n\n\nsudo vnstat --remove --iface <network adapter to remove> --force" 25 80
 
 ######################## install nginx
+clear
+echo "installing nginx"
+sleep 2
 sudo apt install nginx
 sudo ufw allow 80/tcp comment 'NTracking'
 sudo chmod -R 757 /var/www
@@ -117,7 +123,7 @@ sudo systemctl restart nginx.service
 whiptail --msgbox --title "installation of nginx webserver complete" "nginx set up complete\n \nport 80 opened in fire wall\n\n\nif you enter this systems ip address into your web browser\nyou should see the NTracking comming soon page\n\nif you are on a local lan no port forwad is required\n\nif it is a cloud node or on a diferent network you will need to make sure there is a port forward setup" 25 80
 ######################## setup cron jobs
 
-echo "*/20 * * * * $USER /bin/bash $HOME/.local/share/ntracking/resources.sh >> $HOME/resources_\$(date +\%Y\%m\%d).log 2>&1" | sudo tee /etc/cron.d/ntracking_resources
+echo "*/20 * * * * $USER /bin/bash $HOME/.local/share/ntracking/resources.sh >> $HOME/.local/share/ntracking_working_folder/local_machine/resources_\$(date +\%Y\%m\%d).log 2>&1" | sudo tee /etc/cron.d/ntracking_resources
 echo "10 0 * * * $USER /bin/bash $HOME/safe-scripts/ntracking_log_rm.sh" | sudo tee /etc/cron.d/ntracking_log_rm
 echo "0 * * * * $USER /bin/bash $HOME/.local/share/ntracking/mtracking/machine_resources.sh" | sudo tee /etc/cron.d/ntracking_mtracking_machine_resources
 echo '5 * * * * $USER /bin/bash $HOME/.local/share/ntracking/execute_steps.sh' | sudo tee /etc/cron.d/ntracking_execute_steps
