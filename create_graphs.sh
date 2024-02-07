ca374829dbda2aa48917afa839977dce52b0e11f
Neik <25412853+neilgcns@users.noreply.github.com>
2024-02-07 05:22:17 +0000

    Update create_graphs.sh

--- create_graphs.sh
+++ create_graphs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-install_dir="$HOME/.local/share/ntracking_working_folder"
+install_dir="${HOME}/.local/share/ntracking"
 
 cd $install_dir
 
 # Activate virtual environment
-source $HOME/.local/share/RPvenv/bin/activate
+source $install_dir/RPvenv/bin/activate
 
-python3 $HOME/.local/share/ntracking/all_graphs.py
+python3 ./all_graphs.py
 
 sleep 1
-python3 $HOME/.local/share/ntracking/node_info.py
+python3 ./node_info.py
