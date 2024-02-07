98b290e53f55eebf78a806e3f6e4662c66387c1e
Neik <25412853+neilgcns@users.noreply.github.com>
2024-02-07 22:32:03 +0000

    Update sync_logs.sh

--- log_rotation/sync_logs.sh
+++ log_rotation/sync_logs.sh
@@ -55,36 +55,36 @@ cd $HOME
 rsync -avz --update $HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s00/
 
 # Synchronize logs from each remote machine to separate subdirectories
-rsync -avz --update system_01:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s01/
-rsync -avz --update system_02:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s02/
-rsync -avz --update system_03:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s03/
-rsync -avz --update system_04:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s04/
-rsync -avz --update system_05:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s05/
-rsync -avz --update system_06:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s06/
-rsync -avz --update system_07:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s07/
-rsync -avz --update system_08:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s08/
-rsync -avz --update system_09:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s09/
-rsync -avz --update system_10:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s10/
-rsync -avz --update system_11:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s11/
-rsync -avz --update system_12:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s12/
-rsync -avz --update system_13:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s13/
-rsync -avz --update system_14:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s14/
-rsync -avz --update system_15:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s15/
-rsync -avz --update system_16:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s16/
-rsync -avz --update system_17:$HOME/.local/sharelocal_machine/resources_*.log $DEST_BASE_DIR/s17/
-rsync -avz --update system_18:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s18/
-rsync -avz --update system_19:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s19/
-rsync -avz --update system_20:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s20/
-rsync -avz --update system_21:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s21/
-rsync -avz --update system_22:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s22/
-rsync -avz --update system_23:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s23/
-rsync -avz --update system_24:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s24/
-rsync -avz --update system_25:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s25/
-rsync -avz --update system_26:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s26/
-rsync -avz --update system_27:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s27/
-rsync -avz --update system_28:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s28/
-rsync -avz --update system_29:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s29/
-rsync -avz --update system_30:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s30/
+rsync -avz --update s01:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s01/
+rsync -avz --update s02:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s02/
+rsync -avz --update s03:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s03/
+rsync -avz --update s04:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s04/
+rsync -avz --update s05:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s05/
+rsync -avz --update s06:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s06/
+rsync -avz --update s07:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s07/
+rsync -avz --update s08:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s08/
+rsync -avz --update s09:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s09/
+rsync -avz --update s10:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s10/
+rsync -avz --update s11:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s11/
+rsync -avz --update s12:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s12/
+rsync -avz --update s13:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s13/
+rsync -avz --update s14:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s14/
+rsync -avz --update s15:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s15/
+rsync -avz --update s16:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s16/
+rsync -avz --update s17:$HOME/.local/sharelocal_machine/resources_*.log $DEST_BASE_DIR/s17/
+rsync -avz --update s18:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s18/
+rsync -avz --update s19:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s19/
+rsync -avz --update s20:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s20/
+rsync -avz --update s21:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s21/
+rsync -avz --update s22:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s22/
+rsync -avz --update s23:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s23/
+rsync -avz --update s24:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s24/
+rsync -avz --update s25:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s25/
+rsync -avz --update s28:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s26/
+rsync -avz --update s27:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s27/
+rsync -avz --update s28:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s28/
+rsync -avz --update s29:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s29/
+rsync -avz --update s30:$HOME/.local/share/local_machine/resources_*.log $DEST_BASE_DIR/s30/
 
 echo "Synchronization script completed."
 
