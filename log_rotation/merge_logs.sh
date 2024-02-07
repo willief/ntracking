1f8bf4281026b79091d3cb379f87c061292b742d
Neik <25412853+neilgcns@users.noreply.github.com>
2024-02-07 05:12:39 +0000

    Update merge_logs.sh

--- log_rotation/merge_logs.sh
+++ log_rotation/merge_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Base directory for logs
-BASE_LOG_DIR="$HOME/.local/share/ntracking_working_folder/logs"
+BASE_LOG_DIR="$HOME/.local/share/ntracking/logs"
 
 # Loop through each machine
 for machine in s00 s01 s02 s03 s04 s05 s06 s07 s08 s09 s10 s11 s12 s13 s14 s15 s16 s17 s18 s19 s20 s21 s22 s23 s24 s25 s26 s27 s28 s29 s30; do
@@ -14,4 +14,4 @@ for machine in s00 s01 s02 s03 s04 s05 s06 s07 s08 s09 s10 s11 s12 s13 s14 s15 s
 done
 
 #delete empty files from non existing machines
-find $HOME/.local/share/ntracking_working_folder/ -type f -empty -print -delete
+find $HOME/.local/share/ntracking/ -type f -empty -print -delete
