991d8ef26677bbd25e32230f205515fa98f23a83
Neik <25412853+neilgcns@users.noreply.github.com>
2024-02-07 21:10:42 +0000

    Update maid.sh

--- maid.sh
+++ maid.sh
@@ -26,7 +26,7 @@ SELECTION=$(whiptail --title "Safe Network Testnet" --radiolist \
 "4" "Get Test Coins" OFF \
 "5" "Start Vdash" OFF \
 "6" "Update all and Restart" OFF \
-"7" "Josh comnet" OFF 3>&1 1>&2 2>&3)
+"7" "setup NTracking & Vdash" OFF 3>&1 1>&2 2>&3)
 
 if [[ $? -eq 255 ]]; then
 exit 0
@@ -159,48 +159,8 @@ rustup update
 sudo apt update -y && sudo apt upgrade -y
 sudo reboot
 
-############################################################################################################################################# comnet
+############################################################################################################################################# setup NTracking & vdash
 elif [[ "$SELECTION" == "7" ]]; then
-NUMBER_NODES=$(whiptail --title "Number of Nodes to start" --inputbox "\nEnter number of nodes" 8 40 $NUMBER_NODES 3>&1 1>&2 2>&3)
-if [[ $? -eq 255 ]]; then
-exit 0
-fi
-
-############################## count nodes directories and close fire wall
-PORTS_TO_CLOSE=$(ls $HOME/.local/share/safe/node | wc -l)
-sudo ufw delete allow $NODE_PORT_FIRST:$(($NODE_PORT_FIRST+$PORTS_TO_CLOSE-1))/tcp comment 'safe nodes'
-############################## Stop Nodes and delete safe folder
-pkill -e safenode
-rm -rf $HOME/.local/share/safe
-rm -rf $HOME/ntracking/*.log
-
-sleep 2
-############################## install client node and vdash and set up comnet  make sure and change download and unzip instructions and peer
-cd
-mkdir comnet
-cd comnet
-wget https://comnet-sn.s3.eu-west-2.amazonaws.com/maidsafe/main/safe-beta-x86_64-unknown-linux-musl.tar.gz
-wget https://comnet-sn.s3.eu-west-2.amazonaws.com/maidsafe/main/safenode-beta-x86_64-unknown-linux-musl.tar.gz
-sleep 1
-tar -xvzf safe-beta-x86_64-unknown-linux-musl.tar.gz
-tar -xvzf safenode-beta-x86_64-unknown-linux-musl.tar.gz
-sleep 1
-rm *.gz
-
-cargo install vdash
-############################## open ports 
-sudo ufw allow $NODE_PORT_FIRST:$(($NODE_PORT_FIRST+$NUMBER_NODES-1))/tcp comment 'safe nodes'
-sleep 2
-############################## start nodes
-for (( c=$NODE_PORT_FIRST; c<=$(($NODE_PORT_FIRST+$NUMBER_NODES-1)); c++ ))
-do 
-   sleep $NODE_START_DELAY && /home/ubuntu/comnet/safenode --peer /ip4/46.101.5.184/tcp/36301/p2p/12D3KooWBVHdavXJqggn396jQQevJvXtjTN1pwb8YqS65wyitTrx --port $c --max_log_files 2 --max_archived_log_files 1 2>&1 > /dev/null & disown
-   echo "starting node on port $c with $NODE_START_DELAY second delay"
-   NODE_START_DELAY=$(($NODE_START_DELAY+30))
-done
-sleep 2
-
-############################# exit to Vdash
-vdash --glob-path "$HOME/.local/share/safe/node/*/logs/safenode.log"
+bash <(curl -s https://raw.githubusercontent.com/safenetforum-community/ntracking/main/setup.sh)
 
 fi
