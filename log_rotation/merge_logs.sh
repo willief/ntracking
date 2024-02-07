1d5d5eab3b8dcf54d2f3d3c3104fdbd3ce5ef566
Neik <25412853+neilgcns@users.noreply.github.com>
2024-02-07 02:51:34 +0000

    Update merge_logs.sh

--- log_rotation/merge_logs.sh
+++ log_rotation/merge_logs.sh
@@ -4,7 +4,7 @@
 BASE_LOG_DIR="$HOME/.local/share/ntracking_working_folder/logs"
 
 # Loop through each machine
-for machine in s s01 s02 s03 s04 s05 s06 s07 s08 s09 s10 s11 s12 s13 s14 s15 s16 s17 s18 s19 s20 s21 s22 s23 s24 s25 s26 s27 s28 s29 s30; do
+for machine in s00 s01 s02 s03 s04 s05 s06 s07 s08 s09 s10 s11 s12 s13 s14 s15 s16 s17 s18 s19 s20 s21 s22 s23 s24 s25 s26 s27 s28 s29 s30; do
     # Define the source directory and the merged log file name
     LOG_DIR="$BASE_LOG_DIR/$machine"
     MERGED_LOG="$HOME/.local/share/ntracking_working_folder/resources_${machine#s}.log" # e.g., resources1.log for system_01
