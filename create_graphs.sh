8cdbae6a566039019e70e137cb0169cd05c6e1cc
Neik <25412853+neilgcns@users.noreply.github.com>
2024-02-07 03:51:35 +0000

    Update create_graphs.sh

--- create_graphs.sh
+++ create_graphs.sh
@@ -10,6 +10,4 @@ source $HOME/.local/share/RPvenv/bin/activate
 python3 $HOME/.local/share/ntracking/all_graphs.py
 
 sleep 1
-python3 $HOME/.local/share/ntracking/all_graphs.py
-
-
+python3 $HOME/.local/share/ntracking/node_info.py
